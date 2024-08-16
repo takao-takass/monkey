5d949d60db691f22b30ac9b1de8270aa28d44c1b
TAKAHIRO TADA <takass0119@yahoo.co.jp>
2024-08-16 22:32:27 +0900

    reset codes.

--- MonkeyDotnet/src/Monkey.AppHost/Configurations/AspireConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Monkey.AppHost.Configurations;
-
-public class AspireConfiguration
-{
-    public const string Section = "AspireConfiguration";
-    public string? PostgresUserName { get; set; }
-    public string? PostgresPassword { get; set; }
-}
--- MonkeyDotnet/src/Monkey.WebApi/Databases/Monkey/Sample/TSample.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Monkey.WebApi.Databases.Monkey.Sample;
-
-public class TSample
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public int Price { get; set; }
-}
